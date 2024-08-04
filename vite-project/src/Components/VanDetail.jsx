6f187576a0188e13273c69fe6f9af63a7706d6f6
DevRamona <r.ingabire@alustudent.com>
2024-08-04 22:12:24 +0200

    new updates

--- vite-project/src/Components/VanDetail.jsx
+++ vite-project/src/Components/VanDetail.jsx
@@ -2,6 +2,7 @@ export default function VanDetail() {
     return (
         <div>
             <h1>Details needed</h1>
+            <div>Different types of vans available</div>
 
         </div>
     )
