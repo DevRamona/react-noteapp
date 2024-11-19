9216228830b7450ec1d01fdcb45299d3850d4ac9
DevRamona <r.ingabire@alustudent.com>
2024-11-19 11:55:31 +0200

    updates

--- vite-project/src/Host/HostVanInfo.jsx
+++ vite-project/src/Host/HostVanInfo.jsx
@@ -5,7 +5,7 @@ export default function HostVanInfo() {
         <div className="flex flex-wrap w-11/12 gap-5">
         <h4>Name: {currentVan.name}</h4>
             <h4>Category: {currentVan.type}</h4>
-            <h4>Description: {currentVan.description}</h4>
+            <h4>Descript: {currentVan.description}</h4>
             <h4>Visibility: public and private</h4>
         </div>
     )
